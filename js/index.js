38dead5994f62f91e41483294e0bade43454149d
davedeveloper <ddave2001@gmail.com>
2020-09-10 13:23:58 +0530

    Created the final version of the website

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var db = firebase.firestore();
+
+
+var submitbtn = document.getElementById('submit');
+submitbtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    var fullName = document.getElementById('name').value;
+    var mobile = document.getElementById('mobile').value;
+    var address = document.getElementById('address').value;
+    var question = document.getElementById('question').value;
+
+    db.collection('users').doc().set({
+        fullName,
+        mobile,
+        address,
+        question,
+    }).then(()=>{location.assign('/index.html')})
+        .catch((e)=>console.log(e))
+})
